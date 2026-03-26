a5d12039533608caa1b6922a97e72c3a3c493e3c
20222812-tech <20222812@eaut.edu.vn>
2026-03-26 16:45:32 +0700

    Delete mvc_nhanvien directory

--- mvc_nhanvien/controller/nhanvienController.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../model/nhanvien.php';
-class nhanvienController {
-    public function index() {
-        $nhanviens = nhanvien::getAllnhanviens();
-        include __DIR__ . '/../view/nhanvien_view.php';
-    }
-}
-?>
--- mvc_nhanvien/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . "/controller/nhanvienController.php";
-$controller = new nhanvienController();
-$controller->index();
-?>
